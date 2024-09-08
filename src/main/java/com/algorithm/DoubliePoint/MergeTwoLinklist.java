088ee2733259debdd866cb791c6db0b62e2eeba8
ZetlJ2004 <1374691379@qq.com>
2024-09-08 15:38:38 +0800

    init

---
+++ src/main/java/com/algorithm/DoubliePoint/MergeTwoLinklist.java
@@ -0,0 +1,44 @@
+package com.algorithm.DoubliePoint;
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // 虚拟头结点
+        ListNode dummy = new ListNode(-1), p = dummy;
+        ListNode p1 = l1, p2 = l2;
+
+        while (p1 != null && p2 != null) {
+            // 比较 p1 和 p2 两个指针
+            // 将值较小的的节点接到 p 指针
+            if (p1.val > p2.val) {
+                p.next = p2;
+                p2 = p2.next;
+            } else {
+                p.next = p1;
+                p1 = p1.next;
+            }
+            // p 指针不断前进
+            p = p.next;
+        }
+
+        if (p1 != null) {
+            p.next = p1;
+        }
+
+        if (p2 != null) {
+            p.next = p2;
+        }
+
+        return dummy.next;
+    }
+}
+
+public class MergeTwoLinklist {
+}
---
+++ src/main/java/com/index.java
@@ -0,0 +1,7 @@
+package com;
+
+public class index {
+    public static void main(String[] args) {
+        System.out.printf("Hello world!\n");
+    }
+}

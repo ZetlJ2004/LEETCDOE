9fd3e6c99dfbb6d30168cff823a8e642d2042e26
ZetlJ2004 <1374691379@qq.com>
2024-09-30 15:00:20 +0800

    btree

---
+++ src/main/java/com/algorithm/BinaryTree/MaxDepth.java
@@ -0,0 +1,17 @@
+package com.algorithm.BinaryTree;
+
+public class MaxDepth {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        // 利用定义，计算左右子树的最大深度
+        int leftMax = maxDepth(root.left);
+        int rightMax = maxDepth(root.right);
+        // 整棵树的最大深度等于左右子树的最大深度取最大值，
+        // 然后再加上根节点自己
+        int res = Math.max(leftMax, rightMax) + 1;
+
+        return res;
+    }
+}

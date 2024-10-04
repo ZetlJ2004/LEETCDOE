29e925b96750e77fad6ecfe2fbab239cf5af8396
ZetlJ2004 <1374691379@qq.com>
2024-10-05 00:05:34 +0800

    optimize

---
+++ src/main/java/com/algorithm/Palindrome/Optimize.java
@@ -0,0 +1,38 @@
+package com.algorithm.Palindrome;
+
+public class Optimize {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow, fast;
+        slow = fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (fast != null)
+            slow = slow.next;
+
+        ListNode left = head;
+        ListNode right = reverse(slow);
+        while (right != null) {
+            if (left.val != right.val)
+                return false;
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+    }
+
+    ListNode reverse(ListNode head) {
+        ListNode pre = null, cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+}

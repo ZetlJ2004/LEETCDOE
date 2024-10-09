919a342d344409b3e6564a8543544de790dd34c6
ZetlJ2004 <1374691379@qq.com>
2024-10-09 21:19:54 +0800

    dif

---
+++ src/main/java/com/algorithm/Difference/GetModifiedArray.java
@@ -0,0 +1,19 @@
+package com.algorithm.Difference;
+
+public class GetModifiedArray {
+    public int[] getModifiedArray(int length, int[][] updates) {
+        // nums 初始化为全 0
+        int[] nums = new int[length];
+        // 构造差分解法
+        Difference df = new Difference(nums);
+
+        for (int[] update : updates) {
+            int i = update[0];
+            int j = update[1];
+            int val = update[2];
+            df.increment(i, j, val);
+        }
+
+        return df.result();
+    }
+}

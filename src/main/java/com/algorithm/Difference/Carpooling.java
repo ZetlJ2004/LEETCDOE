ba0059586e5347212cd76b99640288522a9f51eb
ZetlJ2004 <1374691379@qq.com>
2024-10-11 21:32:00 +0800

    crop

---
+++ src/main/java/com/algorithm/Difference/Carpooling.java
@@ -0,0 +1,36 @@
+package com.algorithm.Difference;
+
+public class Carpooling {
+    public boolean carPooling(int[][] trips, int capacity) {
+        // 最多有 1001 个车站
+        int[] nums = new int[1001];
+
+        // 构造差分解法
+        Difference df = new Difference(nums);
+
+        for (int[] trip : trips) {
+            // 乘客数量
+            int val = trip[0];
+
+            // 第 trip[1] 站乘客上车
+            int i = trip[1];
+
+            // 第 trip[2] 站乘客已经下车，
+            // 即乘客在车上的区间是 [trip[1], trip[2] - 1]
+            int j = trip[2] - 1;
+
+            // 进行区间操作
+            df.increment(i, j, val);
+        }
+
+        int[] res = df.result();
+
+        // 客车自始至终都不应该超载
+        for (int i = 0; i < res.length; i++) {
+            if (capacity < res[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

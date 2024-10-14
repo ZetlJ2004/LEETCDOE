cadc6a85bb11fa77c4ce5b102b733df9ba7a80c8
ZetlJ2004 <1374691379@qq.com>
2024-10-14 17:51:21 +0800

    numk]

---
+++ src/main/java/com/algorithm/BinaryTree/NumKLessELEMENT.java
@@ -0,0 +1,30 @@
+package com.algorithm.BinaryTree;
+
+public class NumKLessELEMENT {
+    int kthSmallest(TreeNode root, int k) {
+        // 利用 BST 的中序遍历特性
+        traverse(root, k);
+        return res;
+    }
+
+    // 记录结果
+    int res = 0;
+    // 记录当前元素的排名
+    int rank = 0;
+    void traverse(TreeNode root, int k) {
+        if (root == null) {
+            return;
+        }
+        traverse(root.left, k);
+
+        // 中序代码位置
+        rank++;
+        if (k == rank) {
+            // 找到第 k 小的元素
+            res = root.val;
+            return;
+        }
+
+        traverse(root.right, k);
+    }
+}

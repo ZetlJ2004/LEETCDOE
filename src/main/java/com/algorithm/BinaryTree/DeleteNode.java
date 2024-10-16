64fa4ac5334490ef68cbd163da3e813bddd2b5ed
ZetlJ2004 <1374691379@qq.com>
2024-10-16 22:11:48 +0800

    delete

---
+++ src/main/java/com/algorithm/BinaryTree/DeleteNode.java
@@ -0,0 +1,32 @@
+package com.algorithm.BinaryTree;
+
+public class DeleteNode {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+        if (root.val == key) {
+            // 这两个 if 把情况 1 和 2 都正确处理了
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+            // 处理情况 3
+            // 获得右子树最小的节点
+            TreeNode minNode = getMin(root.right);
+            // 删除右子树最小的节点
+            root.right = deleteNode(root.right, minNode.val);
+            // 用右子树最小的节点替换 root 节点
+            minNode.left = root.left;
+            minNode.right = root.right;
+            root = minNode;
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        }
+        return root;
+    }
+
+    TreeNode getMin(TreeNode node) {
+        // BST 最左边的就是最小的
+        while (node.left != null) node = node.left;
+        return node;
+    }
+}

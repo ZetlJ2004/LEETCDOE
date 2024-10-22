1f7c1a96946fe82ce409fbf550e8cc51fbc2beef
ZetlJ2004 <1374691379@qq.com>
2024-10-22 19:48:21 +0800

    adtre

---
+++ src/main/java/com/algorithm/BinaryTree/AddTree.java
@@ -0,0 +1,22 @@
+package com.algorithm.BinaryTree;
+
+public class AddTree {
+    TreeNode convertBST(TreeNode root) {
+        traverse(root);
+        return root;
+    }
+
+    // 记录累加和
+    int sum = 0;
+    void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        traverse(root.right);
+        // 维护累加和
+        sum += root.val;
+        // 将 BST 转化成累加树
+        root.val = sum;
+        traverse(root.left);
+    }
+}

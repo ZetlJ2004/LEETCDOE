aeca4dee558446abe1de40bdf2f42873710dbbe6
ZetlJ2004 <1374691379@qq.com>
2024-10-29 16:51:17 +0800

    crenew

--- src/main/java/com/algorithm/BinaryTree/SearchTreeSearch.java
+++ src/main/java/com/algorithm/BinaryTree/SearchTreeSearch.java
@@ -1,2 +1,19 @@
-package com.algorithm.BinaryTree;public class SearchTreeSearch {
+package com.algorithm.BinaryTree;
+
+public class SearchTreeSearch {
+    TreeNode searchBST(TreeNode root, int target) {
+        if (root == null) {
+            return null;
+        }
+        // 去左子树搜索
+        if (root.val > target) {
+            return searchBST(root.left, target);
+        }
+        // 去右子树搜索
+        if (root.val < target) {
+            return searchBST(root.right, target);
+        }
+        // 当前节点就是目标值
+        return root;
+    }
 }
--- src/main/java/com/algorithm/Map/CanFinish.java
+++ src/main/java/com/algorithm/Map/CanFinish.java
@@ -1,2 +1,62 @@
-package com.algorithm.Map;public class CanFinish {
+package com.algorithm.Map;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class CanFinish {
+    // 记录递归堆栈中的节点
+    boolean[] onPath;
+    // 记录图中是否有环
+    boolean hasCycle = false;
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer>[] graph = buildGraph(numCourses, prerequisites);
+
+        onPath = new boolean[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            // 遍历图中的所有节点
+            traverse(graph, i);
+        }
+        // 只要没有循环依赖可以完成所有课程
+        return !hasCycle;
+    }
+
+    // 图遍历函数，遍历所有路径
+    void traverse(List<Integer>[] graph, int s) {
+        if (hasCycle) {
+            // 如果已经找到了环，也不用再遍历了
+            return;
+        }
+
+        if (onPath[s]) {
+            // s 已经在递归路径上，说明成环了
+            hasCycle = true;
+
+            return;
+        }
+
+        // 前序代码位置
+        onPath[s] = true;
+        for (int t : graph[s]) {
+            traverse(graph, t);
+        }
+        // 后序代码位置
+        onPath[s] = false;
+    }
+
+    List<Integer>[] buildGraph(int numCourses, int[][] prerequisites) {
+        // 图中共有 numCourses 个节点
+        List<Integer>[] graph = new LinkedList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new LinkedList<>();
+        }
+        for (int[] edge : prerequisites) {
+            int from = edge[1], to = edge[0];
+            // 添加一条从 from 指向 to 的有向边
+            // 边的方向是「被依赖」关系，即修完课程 from 才能修课程 to
+            graph[from].add(to);
+        }
+        return graph;
+    }
 }
